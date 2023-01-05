fcac4d35991e97656d0f44bf2a8461b12892f276
ThomasRgl <thomasroglin@free.fr>
2023-01-05 10:19:00 +0100

    link convolution and neural network
    from: Thomas ROGLIN
    
    NOT WORKING YET - DO NOT MERGE
    
    main.c : the main function does :
                - count the number of image in the dataset
                - call the convolution function on each image
                    and store the result in the "dataset" array
                - start the training :
                    - shuffle the dataset array
                    - fill the first layer with the image convolution result
                    - train with NN

--- src/main.c
+++ src/main.c
@@ -17,6 +17,7 @@ k iterate through the Outputs
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h> 
 
 
 #include "type.h"
@@ -26,23 +27,81 @@ k iterate through the Outputs
 #define NB_MAX_LAYER   50
 #define NB_MAX_OUTPUTS 50
 
+typedef struct {
+    char * filename;
+    int value ;
+    f64 * inputs;
+} mri_image;
+
 int main(int argc, char *argv[])
 {
 
-    if(argc < 2)
-    {
-        printf("no input file\n");
-        return 1;
+    // if(argc < 2)
+    // {
+    //     printf("no input file\n");
+    //     return 1;
+    // }
+    // 
+    srand(time(NULL));
+
+    char * dirs[] = { "dataset/Axial/AD", "dataset/Axial/CN"};
+    int counter = 0; 
+
+    // number of images
+    for( int i = 0; i < 2; i++ ){
+        DIR *d;
+        struct dirent *dir;
+        d = opendir(dirs[i]);
+        printf("%s\n", dirs[i]);
+        if (d) {
+            while ((dir = readdir(d)) != NULL) {
+                counter++;
+                // char * filename = dir->d_name;
+                // printf("%s\n", dir->d_name);
+                
+            }
+            closedir(d);
+        }
     }
 
 
-    printf(" id ; err\n");
-    srand(time(NULL));
+    // fill dataset
+    mri_image * dataset = malloc( counter * sizeof(mri_image) );
+    u64 * random_pattern = malloc( counter );
+   
+    u64 n = 0;
+    for( int i = 0; i < 2; i++ ){
+        DIR *d;
+        struct dirent *dir;
+        d = opendir(dirs[i]);
+        if (d) {
+            while ((dir = readdir(d)) != NULL) {
+                dataset[n].inputs = (f64 *) prepare_image(dir->d_name);
+                dataset[n].value = i;
+                dataset[n].filename = dir->d_name;
+
+                // char * filename = dir->d_name;
+                // printf("%s\n", dir->d_name);
+                n++;
+            }
+            closedir(d);
+        }
+    }
+
     
+    
+
+
+        // int * input = process_img(argv[img_iter]);
+
+
+    
+    //  Initialise The NN
     f64 cumul_err = 0.0f;
+    f64 err = 0.0; 
 
     u64 nb_layers = 4;
-    u64 neurons_per_layers[NB_MAX_LAYER] = {3,6,4,1,1};
+    u64 neurons_per_layers[NB_MAX_LAYER] = {10,8,4,1,1};
     Layer ** layers = malloc( nb_layers * sizeof(Layer *) );
 
     f64 expected[NB_MAX_OUTPUTS];
@@ -52,49 +111,39 @@ int main(int argc, char *argv[])
         // printf("%lld, %x\n", i, layers[i] );
     }
 
-
     for(u64 i = 0; i < nb_layers - 1; i++){
         initLayer( layers[i], neurons_per_layers[i+1] );
     }
 
-    // for each images
-    for (size_t img_iter = 1 ; img_iter < argc ; img_iter++) {
+    printf(" id ; err\n");
 
-        printf("\nImage %s :\n", argv[img_iter]);
+    //  Trainsing
+    for( u64 train_id = 0; train_id < 1000; train_id++ ){
+        
+        //randomize dataset
+        for( u64 p = 0 ; p < counter ; p++ ) {    /* randomize order of training patterns */
+            random_pattern[p] = p ;
+        }
 
-        int * input = process_img(argv[img_iter]);
+        for( u64 p = 0 ; p < counter - 1 ; p++) {
+            u64 np = rand() % ( counter - p) + p;
+            // np = p + rando() * ( NumPattern + 1 - p ) ;
 
-        u64 train_id = 0;
-        while( train_id < 4000000){
-            
-            train_id ++;
-            int a = rand() % 10 ; // random betwenn 0 and 10 
-            int b = rand() % 10 ; // random betwenn 0 and 10 
-            int c = 0.0;
-            f64 err = 0.0; 
+            u64 op = random_pattern[p] ;
+            random_pattern[p] = random_pattern[np];
+            random_pattern[np] = op;
+        }
 
-            
-            // setting inputs and expected output
-            if( ( (f64) rand() / (f64) RAND_MAX )  > 0.5 ){
-                c = a + b;
-                expected[0] = 1.0;
-                
-            }
-            else{
-                do {
-                    c = rand() % 19 ; // random betwenn 0 and 10 
-                    }
-                while(c == a + b);
-                expected[0] = 0.0;
-            }
-            
-            
-            //reduce each numbers to a value between -4 and 4, and use activation function on it.
-            layers[0]->neurons[0] = sigmoid( ((f64) a - 4.5) / 2 );
-            layers[0]->neurons[1] = sigmoid( ((f64) b - 4.5) / 2 );
-            layers[0]->neurons[2] = sigmoid( ((f64) c - 9) / 4 );
 
-            
+        for( u64 np = 0 ; np < counter ; np++ ) {
+            u64 p = random_pattern[np];
+
+
+            for( u64 i = 0; i < 10; i++ ){
+                layers[0]->neurons[i] = sigmoid( dataset[p].inputs[i] );
+//             layers[0]->neurons[0] = sigmoid( ((f64) a - 4.5) / 2 );
+            }        
+
             // compute
             for(u64 i = 0; i < nb_layers - 1; i++){
                 computeLayer( layers[i], layers[i+1] );
@@ -108,8 +157,8 @@ int main(int argc, char *argv[])
 
             //error
             if( train_id % 1000 == 0){
-            // if( 1 == 1){
-                //printf(" %llu; %lf\n", train_id, cumul_err/1000);
+            // if( 1 == 1)
+                printf(" %llu; %lf\n", train_id, cumul_err/1000);
                 cumul_err = 0;
                 // getchar();
             }
@@ -122,27 +171,96 @@ int main(int argc, char *argv[])
             for(u64 i = 0; i < nb_layers - 1; i++){
                 backpropagate( layers[i], layers[i+1] );
             }
-        
-
-
-            continue;
-
-            printf("####################################\n");
-            for(u64 i = 0; i < nb_layers; i++){
-                u64 nextsize;
-                if( i < nb_layers - 1){
-                    nextsize = layers[i+1]->size;
-                }
-                else{
-                    nextsize = 0;
-                }
-                // printf("i: %lld\n", i);
-                // debug( layers[i], nextsize );
-            }
-
-            getchar();
-
         }
-    } 
+    }
 }
+
+    // for each images
+    // for (size_t img_iter = 1 ; img_iter < argc ; img_iter++) {
+    //
+    //     printf("\nImage %s :\n", argv[img_iter]);
+    //
+    //     // int * input = process_img(argv[img_iter]);
+    //
+    //     u64 train_id = 0;
+    //     while( train_id < 4000000){
+    //         
+    //         train_id ++;
+    //         int a = rand() % 10 ; // random betwenn 0 and 10 
+    //         int b = rand() % 10 ; // random betwenn 0 and 10 
+    //         int c = 0.0;
+        // f64 err = 0.0; 
+    //
+    //         
+    //         // setting inputs and expected output
+    //         if( ( (f64) rand() / (f64) RAND_MAX )  > 0.5 ){
+    //             c = a + b;
+    //             expected[0] = 1.0;
+    //             
+    //         }
+    //         else{
+    //             do {
+    //                 c = rand() % 19 ; // random betwenn 0 and 10 
+    //                 }
+    //             while(c == a + b);
+    //             expected[0] = 0.0;
+    //         }
+    //         
+    //         
+    //         //reduce each numbers to a value between -4 and 4, and use activation function on it.
+//             layers[0]->neurons[0] = sigmoid( ((f64) a - 4.5) / 2 );
+//             layers[0]->neurons[1] = sigmoid( ((f64) b - 4.5) / 2 );
+//             layers[0]->neurons[2] = sigmoid( ((f64) c - 9) / 4 );
+//
+//             
+//             // compute
+//             for(u64 i = 0; i < nb_layers - 1; i++){
+//                 computeLayer( layers[i], layers[i+1] );
+//             }
+//             
+//             //delta
+//             err = computeOutputDelta( layers[nb_layers - 1] , expected );
+//
+//             cumul_err += err;
+//
+//
+//             //error
+//             if( train_id % 1000 == 0){
+//             // if( 1 == 1){
+//                 //printf(" %llu; %lf\n", train_id, cumul_err/1000);
+//                 cumul_err = 0;
+//                 // getchar();
+//             }
+//
+//             for(u64 i = nb_layers - 2; i > 0; i--){
+//                 // printf("%lld %lld\n", i, i+1);
+//                 computeDelta( layers[i], layers[i+1] );
+//             }
+//
+//             for(u64 i = 0; i < nb_layers - 1; i++){
+//                 backpropagate( layers[i], layers[i+1] );
+//             }
+//         
+//
+//
+//             continue;
+//
+//             printf("####################################\n");
+//             for(u64 i = 0; i < nb_layers; i++){
+//                 u64 nextsize;
+//                 if( i < nb_layers - 1){
+//                     nextsize = layers[i+1]->size;
+//                 }
+//                 else{
+//                     nextsize = 0;
+//                 }
+//                 // printf("i: %lld\n", i);
+//                 // debug( layers[i], nextsize );
+//             }
+//
+//             getchar();
+//
+//         }
+//     } 
+// }
 
